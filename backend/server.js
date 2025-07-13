bf48c42a7c8f87cd0452dd0021749537042a3c5c
Ivan Morozov <vanyam.vvo@gmail.com>
2025-07-13 13:09:20 +1000

    basic api

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const crypto = require('crypto');
 const app = express();
 const PORT = 3000;
 
-app.get("*", (req, resp)=>{
-    resp.send("<h1>Hello world</h1>");
+const messages = [{
+    uuid: crypto.randomUUID(),
+    content: 'First message'
+}];
+
+app.use(express.json());
+
+app.get("/api/messages", async(_, resp)=>{
+    resp.send(messages);
+});
+
+app.post("/api/messages", async(req, resp)=>{
+
+    if (!req.body || !req.body.content) return resp.sendStatus(400);
+    const uuid = crypto.randomUUID();
+    const content = req.body.content;
+    
+    messages.push({uuid: uuid, content: content});
+    resp.status(200).send(`UUID: ${uuid}, content: ${content}`);
+
+});
+
+app.use(express.static(__dirname + "/public/"));
+
+app.get('*', (_, resp) => {
+  resp.status(404).sendFile(__dirname + "/notFound.html");
 });
 
 app.listen(PORT, ()=>{
